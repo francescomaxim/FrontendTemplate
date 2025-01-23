5472ee8287504d12e270c1a975a209efcbccd4d4
francesco.maxim <francesco.maxim@icloud.com>
2025-01-23 02:11:43 +0200

    demo for sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ChangeDetectorRef, Input, signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
+  @Input() isMobileView!: boolean;
+  isOpen = signal<true | false>(false); // Sidebar-ul este inițial închis
 
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  toggleSidebar() {
+    this.isOpen.set(!this.isOpen());
+    this.cdr.detectChanges();
+  }
 }
