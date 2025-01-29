a87ff9abd058168142414902a5e6d176e6765384
francesco.maxim <francesco.maxim@icloud.com>
2025-01-30 00:18:38 +0200

    translate future

--- src/app/components/language-switcher/language-switcher.component.ts
+++ src/app/components/language-switcher/language-switcher.component.ts
@@ -1,5 +1,9 @@
 import { Component, inject, input } from '@angular/core';
 import { LanguageSwitcherService } from './language-switcher.service';
+import { HeaderConfigModel } from '../../../assets/models/header-config.model';
+import { SideMenuConfigModel } from '../../../assets/models/side-menu-config.model';
+import { HeroConfigModel } from '../../../assets/models/hero-config.model';
+import { FooterConfigModel } from '../../../assets/models/footer-config.model';
 
 @Component({
   selector: 'app-language-switcher',
@@ -10,12 +14,22 @@ import { LanguageSwitcherService } from './language-switcher.service';
 export class LanguageSwitcherComponent {
   private languageService = inject(LanguageSwitcherService);
 
-  selectedLanguage: string = 'EN';
+  selectedLanguage: string = 'RO';
 
-  // headerData = input.required<HeaderModel>();
+  headerData = input.required<HeaderConfigModel>();
+  sideMenuData = input.required<SideMenuConfigModel>();
+  heroData = input.required<HeroConfigModel>();
+  footerData = input.required<FooterConfigModel>();
 
   selectLanguage(language: string): void {
     this.selectedLanguage = language;
-    // this.languageService.translate(this.headerData, this.selectedLanguage);
+    console.log(this.headerData());
+    this.languageService.translate(
+      this.headerData,
+      this.sideMenuData,
+      this.heroData,
+      this.footerData,
+      this.selectedLanguage
+    );
   }
 }
