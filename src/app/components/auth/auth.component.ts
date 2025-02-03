1036164865a57477e11d604199419d0ad33895ff
francesco.maxim <francesco.maxim@icloud.com>
2025-02-03 08:52:01 +0200

    test

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,8 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { AuthResponseData, AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
 })
@@ -10,7 +14,43 @@ export class AuthComponent {
   //login = true/ signup=false
   authMode = signal<true | false>(true);
 
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
   switchAuthMode() {
     this.authMode.set(!this.authMode());
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('hello');
+
+    // if (!form.valid) {
+    //   return;
+    // }
+    // const email = form.value.email;
+    // const password = form.value.password;
+
+    // let authObs: Observable<AuthResponseData>;
+
+    // if (this.authMode()) {
+    //   authObs = this.authService.login(email, password);
+    // } else {
+    //   authObs = this.authService.signup(email, password);
+    // }
+
+    // authObs.subscribe(
+    //   (resData) => {
+    //     console.log(resData);
+    //     // this.isLoading = false;
+    //     this.router.navigate(['/home']);
+    //   },
+    //   (errorMessage) => {
+    //     console.log(errorMessage);
+    //     // this.error = errorMessage;
+    //     // this.isLoading = false;
+    //   }
+    // );
+
+    // form.reset();
   }
 }
