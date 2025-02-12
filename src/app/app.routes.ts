b49c8daa0e172d7d4013812d8792636fcf02a295
francesco.maxim <francesco.maxim@icloud.com>
2025-02-12 19:15:50 +0200

    added other empty pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { HeroComponent } from './components/hero/hero.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
   {
@@ -10,12 +14,28 @@ export const routes: Routes = [
     component: HomeComponent,
   },
   {
+    path: 'auth',
+    component: AuthComponent,
+  },
+  {
     path: 'home',
     component: HomeComponent,
   },
   {
-    path: 'auth',
-    component: AuthComponent,
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'services',
+    component: ServicesComponent,
+  },
+  {
+    path: 'products',
+    component: ProductsComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
   },
   {
     path: '**',
