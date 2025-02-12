970568175a827aa7c87cd4be6ef07a8baaf7a78f
francesco.maxim <francesco.maxim@icloud.com>
2025-02-12 19:12:14 +0200

    added empty home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,16 @@ import { Routes } from '@angular/router';
 import { HeroComponent } from './components/hero/hero.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HeroComponent,
+    component: HomeComponent,
   },
   {
     path: 'home',
-    component: HeroComponent,
+    component: HomeComponent,
   },
   {
     path: 'auth',
